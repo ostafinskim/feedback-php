e6cf94c781ad1b2e36942d932065a036fe8779e9
Miro Ostafiński <ostafinskim@gmail.com>
2022-10-26 11:03:15 +0100

    feedback app

---
+++ feedback.php
@@ -0,0 +1,24 @@
+<?php include 'inc/header.php'; ?>
+
+<?php 
+  $sql = mysqli_query($conn, "SELECT * FROM feedback");
+  $results = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+?>
+
+<h2>Past Feedback</h2>
+
+<?php if(empty($results)): ?>
+<p class="lead mt-3">There is no feedback</p>
+<?php endif; ?>
+
+<?php foreach($results as $item): ?>
+<div class="card my-3 w-75">
+  <div class="card-body text-center">
+    <?php echo $item['body']; ?>
+    <div class="text-secondary mt-2">
+      By <?php echo $item['name']; ?> on <?php echo $item['date']; ?>
+    </div>
+  </div>
+</div>
+<?php endforeach; ?>
+<?php include 'inc/footer.php'; ?>
